7f37346b06bec0a33f1e9ff055ad224499d7617a
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-06-18 22:43:05 +0400

    Data structures done

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,13 @@
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for row in matrix:
-        for i, element in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(element))
-            else:
-                print("{:d}".format(element), end=" ") 
+        if len(row) == 0:
+            print()
+        else:
+            for i, element in enumerate(row):
+                if i == len(row) - 1:
+                    print("{:d}".format(element))
+                else:
+                    print("{:d}".format(element), end=" ") 
                 
                 
