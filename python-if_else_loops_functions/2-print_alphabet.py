7b260c1fab82c1da5d8ae8e64ec6f523b4400fab
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-06-18 19:48:33 +0400

    Modules done

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+# The range function generates a sequence of numbers from 97 to 123 (inclusive)
+# These numbers correspond to the ASCII values of lowercase letters a to z
 for i in range(97, 123):
+    # The chr function converts an ASCII value to its corresponding character
+    # The format function is used to insert the character into the string
     print("{}".format(chr(i)), end="")
